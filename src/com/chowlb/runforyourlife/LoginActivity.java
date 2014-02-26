d22ef6f161e0a6287af973b7aee253a2842f9881
chowlb <chowlb.stone@gmail.com>
2014-02-26 15:38:22 -0500

    added register activity and map activity

--- src/com/chowlb/runforyourlife/LoginActivity.java
+++ src/com/chowlb/runforyourlife/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.chowlb.runforyourlife;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity{
 
@@ -23,14 +30,32 @@ public class LoginActivity extends Activity{
 		password = (EditText) findViewById(R.id.passwordET);
 		Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/28daysfont.ttf");
 		logo.setTypeface(typeFace);
-		username.setTypeface(typeFace);
-		password.setTypeface(typeFace);
 		Button loginButton = (Button) findViewById(R.id.loginButton);
 		loginButton.setTypeface(typeFace);
+		Button registerButton = (Button) findViewById(R.id.registerButton);
+		registerButton.setTypeface(typeFace);
+		
+		final ConnectivityManager conMgr =  (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		final NetworkInfo activeNetwork = conMgr.getActiveNetworkInfo();
+		if (activeNetwork != null && activeNetwork.isConnected()) {
+		    //notify user you are online
+		} else {
+			Toast.makeText(this, "No Network Access Found" , Toast.LENGTH_LONG).show();
+			startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+		} 
 		
 	    
 	}
 	
+	public void login(View v) {
+		Intent intent = new Intent(this, MapActivity.class);
+		startActivity(intent);
+	}
+	
+	public void register(View v) {
+		Intent intent = new Intent(this, RegisterActivity.class);
+		startActivity(intent);
+	}
 	
 	@Override
 	  protected void onPause() {
--- src/com/chowlb/runforyourlife/SplashScreen.java
+++ src/com/chowlb/runforyourlife/SplashScreen.java
@@ -2,18 +2,28 @@ package com.chowlb.runforyourlife;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
  
 public class SplashScreen extends Activity {
  
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 3000;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
+        TextView logo = (TextView) findViewById(R.id.chowlbLogoTV);
+		
+		Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/pixelfont.ttf");
+		logo.setTypeface(typeFace);
+		
         
         new Handler().postDelayed(new Runnable() {
         	
